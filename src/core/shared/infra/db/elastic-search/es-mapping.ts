b2107f33c5b33c462909da731126c27002bd5099
netoudi <netoudi@rocketmail.com>
2025-02-11 08:51:01 -0300

    criando-mapeamento-dos-dados-de-categoria

---
+++ src/core/shared/infra/db/elastic-search/es-mapping.ts
@@ -0,0 +1,21 @@
+import { MappingTypeMapping } from '@elastic/elasticsearch/api/types';
+
+export const esMapping: MappingTypeMapping = {
+  properties: {
+    type: {
+      type: 'keyword',
+    },
+    category_name: {
+      type: 'keyword',
+    },
+    category_description: {
+      type: 'text',
+    },
+    is_active: {
+      type: 'boolean',
+    },
+    created_at: {
+      type: 'date',
+    },
+  },
+};
