5b6d30330f96654e82c51d6ed00c519f9e45cd9f
netoudi <netoudi@rocketmail.com>
2025-02-11 14:22:49 -0300

    construindo-todas-as-consultas-do-repositorio-de-categoria

--- src/core/category/infra/db/elastic-search/__tests__/category-elastic-search.repository.int-spec.ts
+++ src/core/category/infra/db/elastic-search/__tests__/category-elastic-search.repository.int-spec.ts
@@ -46,4 +46,67 @@ describe('CategoryElasticSearchRepository Integration Tests', () => {
     expect(entities[0].toJSON()).toStrictEqual(categories[0].toJSON());
     expect(entities[1].toJSON()).toStrictEqual(categories[1].toJSON());
   });
+
+  it('should find a entity by id', async () => {
+    const category = Category.create({
+      id: new CategoryId(),
+      name: 'Movie',
+      description: 'Some description',
+      is_active: false,
+      created_at: new Date(),
+    });
+    await repository.insert(category);
+    const output = await repository.findById(category.id);
+    expect(output?.toJSON()).toStrictEqual(category.toJSON());
+    expect(repository.findById(new CategoryId())).resolves.toBeNull();
+  });
+
+  it('should find a entity by filter', async () => {
+    const category = Category.create({
+      id: new CategoryId(),
+      name: 'Movie',
+      description: 'Some description',
+      is_active: false,
+      created_at: new Date(),
+    });
+    await repository.insert(category);
+    let output = await repository.findOneBy({ id: category.id });
+    expect(output?.toJSON()).toStrictEqual(category.toJSON());
+    expect(repository.findOneBy({ is_active: true })).resolves.toBeNull();
+    output = await repository.findOneBy({ id: category.id, is_active: false });
+    expect(output?.toJSON()).toStrictEqual(category.toJSON());
+  });
+
+  it('should find entities by filter an order', async () => {
+    const categories = [
+      Category.fake().aCategory().withName('a').build(),
+      Category.fake().aCategory().withName('b').build(),
+    ];
+    await repository.bulkInsert(categories);
+    const output = await repository.findBy({ is_active: true }, { field: 'name', direction: 'asc' });
+    expect(output).toStrictEqual([categories[0], categories[1]]);
+  });
+
+  it('should find all entities', async () => {
+    const category = new Category({
+      id: new CategoryId(),
+      name: 'Movie',
+      description: 'Some description',
+      is_active: false,
+      created_at: new Date(),
+    });
+    await repository.insert(category);
+    const output = await repository.findAll();
+    expect(output).toHaveLength(1);
+    expect(JSON.stringify(output)).toBe(JSON.stringify([category]));
+  });
+
+  it('should return a entities list by ids', async () => {
+    const categories = Category.fake().theCategories(2).build();
+    await repository.bulkInsert(categories);
+    const output = await repository.findByIds(categories.map((category) => category.id));
+    expect(output.exists).toHaveLength(2);
+    expect(output.not_exists).toHaveLength(0);
+    expect(JSON.stringify(output.exists)).toBe(JSON.stringify(categories));
+  });
 });
--- src/core/category/infra/db/elastic-search/category-elastic-search.repository.ts
+++ src/core/category/infra/db/elastic-search/category-elastic-search.repository.ts
@@ -1,3 +1,4 @@
+import { GetGetResult, QueryDslQueryContainer } from '@elastic/elasticsearch/api/types';
 import { ElasticsearchService } from '@nestjs/elasticsearch';
 import { Category, CategoryId } from '@/core/category/domain/category.entity';
 import {
@@ -5,11 +6,16 @@ import {
   CategorySearchResult,
   ICategoryRepository,
 } from '@/core/category/domain/category.repository';
-import { CategoryElasticSearchMapper } from '@/core/category/infra/db/elastic-search/category-elastic-search.mapper';
+import {
+  CATEGORY_DOCUMENT_TYPE_NAME,
+  CategoryDocument,
+  CategoryElasticSearchMapper,
+} from '@/core/category/infra/db/elastic-search/category-elastic-search.mapper';
 import { SortDirection } from '@/core/shared/domain/repository/search-params';
 
 export class CategoryElasticSearchRepository implements ICategoryRepository {
   sortableFields: string[] = ['name', 'created_at'];
+  sortableFieldsMap: Record<string, string> = { name: 'category_name', created_at: 'created_at' };
 
   constructor(
     private esClient: ElasticsearchService,
@@ -49,23 +55,153 @@ export class CategoryElasticSearchRepository implements ICategoryRepository {
   }
 
   async findById(entityId: CategoryId): Promise<Category | null> {
-    throw new Error('Method not implemented.');
+    const result = await this.esClient.search({
+      index: this.index,
+      body: {
+        query: {
+          bool: {
+            must: [
+              {
+                match: {
+                  _id: entityId.value,
+                },
+              },
+              {
+                match: {
+                  type: CATEGORY_DOCUMENT_TYPE_NAME,
+                },
+              },
+            ],
+          },
+        },
+      },
+    });
+    const docs = result.body.hits.hits as GetGetResult<CategoryDocument>[];
+    if (docs.length === 0 || docs[0]._source === undefined) {
+      return null;
+    }
+    return CategoryElasticSearchMapper.toEntity(entityId.value, docs[0]._source);
   }
 
   async findOneBy(filter: Partial<Category>): Promise<Category | null> {
-    throw new Error('Method not implemented.');
+    const query: QueryDslQueryContainer = {
+      bool: {
+        must: [
+          {
+            match: {
+              type: CATEGORY_DOCUMENT_TYPE_NAME,
+            },
+          },
+        ],
+      },
+    };
+    if (filter.id !== undefined) {
+      // @ts-expect-error - must is an array
+      query.bool.must.push({
+        match: {
+          _id: filter.id.value,
+        },
+      });
+    }
+    if (filter.is_active !== undefined) {
+      // @ts-expect-error - must is an array
+      query.bool.must.push({
+        match: {
+          is_active: filter.is_active,
+        },
+      });
+    }
+    const result = await this.esClient.search({
+      index: this.index,
+      body: {
+        query,
+      },
+    });
+    const docs = result.body.hits.hits as GetGetResult<CategoryDocument>[];
+    if (docs.length === 0 || docs[0]._source === undefined) {
+      return null;
+    }
+    return CategoryElasticSearchMapper.toEntity(docs[0]._id as string, docs[0]._source);
   }
 
   async findBy(filter: Partial<Category>, order?: { field: string; direction: SortDirection }): Promise<Category[]> {
-    throw new Error('Method not implemented.');
+    const query: QueryDslQueryContainer = {
+      bool: {
+        must: [
+          {
+            match: {
+              type: CATEGORY_DOCUMENT_TYPE_NAME,
+            },
+          },
+        ],
+      },
+    };
+    if (filter.id !== undefined) {
+      // @ts-expect-error - must is an array
+      query.bool.must.push({
+        match: {
+          _id: filter.id.value,
+        },
+      });
+    }
+    if (filter.is_active !== undefined) {
+      // @ts-expect-error - must is an array
+      query.bool.must.push({
+        match: {
+          is_active: filter.is_active,
+        },
+      });
+    }
+    const result = await this.esClient.search({
+      index: this.index,
+      body: {
+        query,
+        sort: order ? [{ [this.sortableFieldsMap[order.field]]: order.direction }] : undefined,
+      },
+    });
+    return result.body.hits.hits.map((doc: any) => CategoryElasticSearchMapper.toEntity(doc._id, doc._source));
   }
 
   async findAll(): Promise<Category[]> {
-    throw new Error('Method not implemented.');
+    const result = await this.esClient.search({
+      index: this.index,
+      body: {
+        query: {
+          match: {
+            type: CATEGORY_DOCUMENT_TYPE_NAME,
+          },
+        },
+      },
+    });
+    return result.body.hits.hits.map((doc: any) => CategoryElasticSearchMapper.toEntity(doc._id, doc._source));
   }
 
   async findByIds(ids: CategoryId[]): Promise<{ exists: Category[]; not_exists: CategoryId[] }> {
-    throw new Error('Method not implemented.');
+    const result = await this.esClient.search({
+      body: {
+        query: {
+          bool: {
+            must: [
+              {
+                ids: {
+                  values: ids.map((id) => id.value),
+                },
+              },
+              {
+                match: {
+                  type: CATEGORY_DOCUMENT_TYPE_NAME,
+                },
+              },
+            ],
+          },
+        },
+      },
+    });
+    const docs = result.body.hits.hits as GetGetResult<CategoryDocument>[];
+    return {
+      exists: docs.map((doc) => CategoryElasticSearchMapper.toEntity(doc._id as string, doc._source!)),
+      not_exists: ids.filter((id) => !docs.some((doc) => doc._id === id.value)),
+    };
   }
 
   async existsById(ids: CategoryId[]): Promise<{ exists: CategoryId[]; not_exists: CategoryId[] }> {
