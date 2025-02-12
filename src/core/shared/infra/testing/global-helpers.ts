7f0790f3ce432401e7ef2a38e087b9c7655a4de4
netoudi <netoudi@rocketmail.com>
2025-02-12 10:27:17 -0300

    reuso-de-containers-parte-2

--- src/core/shared/infra/testing/global-helpers.ts
+++ src/core/shared/infra/testing/global-helpers.ts
@@ -7,14 +7,30 @@ global.fail = (message: string) => {
   throw new Error(message);
 };
 
+async function tryStartContainer<T>(fn: () => Promise<T>): Promise<T> {
+  do {
+    try {
+      return await fn();
+    } catch (error) {
+      if (!error.message.includes('port is already allocated')) {
+        throw error;
+      }
+    }
+  } while (true);
+}
+
 export function setupElasticsearch() {
   let _esClient: ElasticsearchService;
   let _startedContainer: StartedElasticsearchContainer;
   let _indexName: string;
 
   beforeAll(async () => {
-    const esContainer = new ElasticsearchContainer('elasticsearch:7.17.7').withReuse();
-    _startedContainer = await esContainer.start();
+    _startedContainer = await tryStartContainer(async () => {
+      return new ElasticsearchContainer('elasticsearch:7.17.7')
+        .withExposedPorts({ container: 9200, host: 9300 })
+        .withReuse()
+        .start();
+    });
   }, 120_000);
 
   beforeEach(async () => {
@@ -25,10 +41,6 @@ export function setupElasticsearch() {
 
   afterEach(async () => {
     await _esClient?.indices?.delete({ index: _indexName });
-  });
-
-  afterAll(async () => {
-    await _startedContainer?.stop();
   });
 
   return {
