8890f7f40fbbb0a0a050787c4f218d15b2207ea9
netoudi <netoudi@rocketmail.com>
2025-02-25 09:29:29 -0300

    construindo-agregado-de-video

---
+++ src/core/video/domain/video.validator.ts
@@ -0,0 +1,28 @@
+import { MaxLength } from 'class-validator';
+import { ClassValidatorFields } from '@/core/shared/domain/validators/class-validator-fields';
+import { Notification } from '@/core/shared/domain/validators/notification';
+import { Video } from '@/core/video/domain/video.aggregate';
+
+export class VideoRules {
+  @MaxLength(255, { groups: ['title'] })
+  title: string;
+
+  constructor(aggregate: Video) {
+    Object.assign(this, aggregate);
+  }
+}
+
+export class VideoValidator extends ClassValidatorFields {
+  validate(notification: Notification, data: Video, fields?: string[]): boolean {
+    const newFields = fields?.length ? fields : ['title'];
+    return super.validate(notification, new VideoRules(data), newFields);
+  }
+}
+
+export class VideoValidatorFactory {
+  static create() {
+    return new VideoValidator();
+  }
+}
+
+export default VideoValidatorFactory;
