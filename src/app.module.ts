2fcd9d2fc4e5d3b8de5f066ed836adb00abeaf3e
netoudi <netoudi@rocketmail.com>
2025-02-26 11:00:00 -0300

    criando-configmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,18 @@ import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { CastMembersModule } from '@/modules/cast-members-module/cast-members.module';
 import { CategoriesModule } from '@/modules/categories-module/categories.module';
+import { ConfigModule } from '@/modules/config-module/config.module';
 import { GenresModule } from '@/modules/genres-module/genres.module';
 import { VideosModule } from '@/modules/videos-module/videos.module';
 
 @Module({
-  imports: [CategoriesModule.forRoot(), GenresModule.forRoot(), CastMembersModule.forRoot(), VideosModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    CategoriesModule.forRoot(),
+    GenresModule.forRoot(),
+    CastMembersModule.forRoot(),
+    VideosModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/config-module/config.module.spec.ts
@@ -0,0 +1,34 @@
+import { ConfigService } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@/modules/config-module/config.module';
+import { overrideConfiguration } from '@/modules/config-module/configuration';
+
+describe('ConfigModule', () => {
+  it('should define the variables', async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
+    }).compile();
+    const configService = moduleRef.get(ConfigService);
+    expect(configService.get('elastic_search.host')).toBeDefined();
+    expect(configService.get('elastic_search.index')).toBeDefined();
+  });
+
+  it('should override variables', async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          load: [
+            overrideConfiguration({
+              elastic_search: {
+                host: 'http://localhost:9200',
+              },
+            }),
+          ],
+        }),
+      ],
+    }).compile();
+    const configService = moduleRef.get(ConfigService);
+    expect(configService.get('elastic_search.host')).toBe('http://localhost:9200');
+    expect(configService.get('elastic_search.index')).toBeDefined();
+  });
+});
