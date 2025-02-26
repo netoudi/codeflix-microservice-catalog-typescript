8b05aacb097bed87f4de60135e5340d2b9968627
netoudi <netoudi@rocketmail.com>
2025-02-26 11:34:54 -0300

    criando-modulo-nest-para-o-elasticsearch

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from '@/app.service';
 import { CastMembersModule } from '@/modules/cast-members-module/cast-members.module';
 import { CategoriesModule } from '@/modules/categories-module/categories.module';
 import { ConfigModule } from '@/modules/config-module/config.module';
+import { ElasticSearchModule } from '@/modules/elastic-search-module/elastic-search-module';
 import { GenresModule } from '@/modules/genres-module/genres.module';
 import { VideosModule } from '@/modules/videos-module/videos.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ElasticSearchModule,
     CategoriesModule.forRoot(),
     GenresModule.forRoot(),
     CastMembersModule.forRoot(),
