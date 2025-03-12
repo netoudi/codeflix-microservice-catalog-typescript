4d19c53d35370c02419a396b613cd5aefa108806
netoudi <netoudi@rocketmail.com>
2025-03-12 08:57:57 -0300

    chamando-casos-de-uso-no-consumer-de-category

--- src/modules/categories-module/categories/categories.consumer.ts
+++ src/modules/categories-module/categories/categories.consumer.ts
@@ -1,16 +1,53 @@
-import { Controller } from '@nestjs/common';
-import { Ctx, EventPattern, KafkaContext, Payload } from '@nestjs/microservices';
+import { Controller, Inject, Logger, ValidationPipe } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
+import { SaveCastMemberInput } from '@/core/cast-member/application/use-cases/save-cast-member/save-cast-member.input';
+import { DeleteCategoryUseCase } from '@/core/category/application/use-cases/delete-category/delete-category.use-case';
+import { SaveCategoryUseCase } from '@/core/category/application/use-cases/save-category/save-category.use-case';
+import { CDCPayloadDto } from '@/modules/kafka-module/cdc.dto';
 
 @Controller()
 export class CategoriesConsumer {
+  private logger = new Logger(CategoriesConsumer.name);
+
+  @Inject(SaveCategoryUseCase)
+  private saveUseCase: SaveCategoryUseCase;
+
+  @Inject(DeleteCategoryUseCase)
+  private deleteUseCase: DeleteCategoryUseCase;
+
   @EventPattern('mysql.micro_videos.categories')
-  handle(@Payload() message, @Ctx() context: KafkaContext) {
-    console.log(
-      message.toString(),
-      context,
-      message.forEach((byte, index) => {
-        console.log(`Byte ${index}: 0x${byte.toString(16).padStart(2, '0')}`);
-      }),
-    );
+  async handle(@Payload(new ValidationPipe()) message: CDCPayloadDto) {
+    switch (message.op) {
+      case 'r':
+        this.logger.log(`[INFO] ${CategoriesConsumer.name} - Discarding read operation`);
+        break;
+      case 'c':
+      case 'u':
+        this.logger.log(
+          `[INFO] [${CategoriesConsumer.name}] - Processing operation ${message.op} - ${JSON.stringify(message.after)}`,
+        );
+        const inputBeforeValidate = {
+          category_id: message.after.id,
+          name: message.after.name,
+          description: message.after.description,
+          is_active: message.after.is_active,
+          created_at: message.after.created_at,
+        };
+        const input = await new ValidationPipe({
+          errorHttpStatusCode: 422,
+          transform: true,
+        }).transform(inputBeforeValidate, {
+          type: 'body',
+          metatype: SaveCastMemberInput,
+        });
+        await this.saveUseCase.execute(input);
+        break;
+      case 'd':
+        this.logger.log(
+          `[INFO] [${CategoriesConsumer.name}] - Processing operation ${message.op} - ${JSON.stringify(message.before)}`,
+        );
+        await this.deleteUseCase.execute({ id: message.before?.category_id });
+        break;
+    }
   }
 }
